82189fc9bc589f7594fef4659191d114766ae89d
Dmitry <mr.dmitriy99@yandex.ru>
2020-02-27 21:58:49 +0300

    Finished Lab1

---
+++ src/main/java/com/bgps/labs/LabsApplication.java
@@ -0,0 +1,13 @@
+package com.bgps.labs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LabsApplication.class, args);
+    }
+
+}
